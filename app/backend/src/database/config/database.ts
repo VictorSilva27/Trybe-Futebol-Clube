abae7de63ff079ea252183e2a9985d04fad5eba1
VictorSilva27 <victoradaosilva2@gmail.com>
2022-12-13 15:53:46 -0300

    Alterando database

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.PGUSER || 'root',
-  password: process.env.PGPASSWORD || '123456',
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  host: process.env.PGHOST || 'localhost',
-  port: Number(process.env.PGPORT) || 3002,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
   dialect: 'postgres',
 }
 
--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  baseURL: 'https://backend-production-3832.up.railway.app/',
 });
 
 export const setToken = (token) => {
